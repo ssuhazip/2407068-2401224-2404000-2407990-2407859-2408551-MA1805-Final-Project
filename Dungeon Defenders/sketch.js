ca843a7e4a4a9129fc459d079f3663896b41d171
hanlacson <161340914+hanlacson@users.noreply.github.com>
2024-02-26 15:32:23 +0000

    Add files via upload

---
+++ Dungeon Defenders/sketch.js
@@ -0,0 +1,46 @@
+//TILEMAP VARIABLES HERE
+
+
+//ITEM VARIABLES GO HERE
+
+
+//ENEMIES VARIABLES GO HERE
+
+
+//PLAYER VARIABLES GO HERE
+
+function preload() {
+    //IMAGES FOR MAP AND CHARACTERS GO HERE
+    //ALL IMAGES PLEASE PUT IN THE ASSETS FOLDER
+}
+
+function setup() {
+    //createCanvas (500, 500)
+
+
+    //console.log IN CASE YOU NEED TO SEE WHAT THE CODE IS DOING/ WHAT IT WILL SHOW
+}
+
+function draw() {
+    //background 
+
+    //for loop tilemap texture graphics
+
+    //for loop enemies or items
+}
+
+class Tile {
+    //code
+}
+
+class Enemies {
+    //code
+}
+
+class Item {
+    //code
+}
+
+class Player {
+    //code
+}
