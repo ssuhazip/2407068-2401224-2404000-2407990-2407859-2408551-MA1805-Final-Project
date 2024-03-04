7ccdbc8ce3dd28ef3d268e10c1a3fad16bdf1c4d
a11y55a <allyssarosed@gmail.com>
2024-03-04 16:21:04 +0000

    added code from main and character image to see if it works

--- Dungeon Defenders/sketch.js
+++ Dungeon Defenders/sketch.js
@@ -1,98 +1,146 @@
-//TILEMAP VARIABLES HERE
+// TILEMAP VARIABLES HERE
+let tilemap = [];
+let numDown = 10;
+let numAcross = 10;
+let tileSize = 50;
 
+let textures = [];
+let graphicsMap = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
 
-//ITEM VARIABLES GO HERE
+// ITEM VARIABLES GO HERE
+// ENEMIES VARIABLES GO HERE
 
-
-//ENEMIES VARIABLES GO HERE
-
-
-//PLAYER VARIABLES GO HERE
+// PLAYER VARIABLES GO HERE
 let characterImg;
 let character;
-let speed = 8; 
+let speed = 8;
 let leftKeyPressed = false;
 let rightKeyPressed = false;
 let upKeyPressed = false;
 let downKeyPressed = false;
 
 function preload() {
-    //IMAGES FOR MAP AND CHARACTERS GO HERE
-    //ALL IMAGES PLEASE PUT IN THE ASSETS FOLDER
+    // IMAGES FOR MAP AND CHARACTERS GO HERE
+    // ALL IMAGES PLEASE PUT IN THE ASSETS FOLDER
     characterImg = loadImage('player.png');
 }
 
 function setup() {
-    //createCanvas (500, 500)
     createCanvas(600, 600);
     character = new Character(width / 5, height / 5, 60); // Character's initial position and size
-    //console.log IN CASE YOU NEED TO SEE WHAT THE CODE IS DOING/ WHAT IT WILL SHOW
+
+    // Setup tilemap
+    let tileID = 0;
+    for (let across = 0; across < numAcross; across++) {
+        tilemap[across] = [];
+        for (let down = 0; down < numDown; down++) {
+            let x = across * tileSize;
+            let y = down * tileSize;
+            let textureNum = graphicsMap[down][across];
+            tilemap[across][down] = new Tile(textures[textureNum], x, y, tileSize, tileID);
+            tileID++;
+        }
+    }
 }
 
 function draw() {
-    //background 
-    background(220);
-    //for loop tilemap texture graphics
-    //for loop enemies or items]
+    background(0);
+
+    // Display tilemap
+    for (let across = 0; across < numAcross; across++) {
+        for (let down = 0; down < numDown; down++) {
+            tilemap[across][down].display();
+            tilemap[across][down].debug();
+        }
+    }
+
+    // Move character
     if (leftKeyPressed && character.x > 0) {
         character.x -= speed;
-      }
-      if (rightKeyPressed && character.x < width - character.size) {
+    }
+    if (rightKeyPressed && character.x < width - character.size) {
         character.x += speed;
-      }
-      if (upKeyPressed && character.y > 0) {
+    }
+    if (upKeyPressed && character.y > 0) {
         character.y -= speed;
-      }
-      if (downKeyPressed && character.y < height - character.size) {
+    }
+    if (downKeyPressed && character.y < height - character.size) {
         character.y += speed;
-}
-character.display();
-}
-
-class Tile {
-    //code
-}
-
-class Enemies {
-    //code
-}
-
-class Item {
-    //code
+    }
+    character.display();
 }
 
 function keyPressed() {
     if (keyCode === LEFT_ARROW) {
-      leftKeyPressed = true;
+        leftKeyPressed = true;
     } else if (keyCode === RIGHT_ARROW) {
-      rightKeyPressed = true;
+        rightKeyPressed = true;
     } else if (keyCode === UP_ARROW) {
-      upKeyPressed = true;
+        upKeyPressed = true;
     } else if (keyCode === DOWN_ARROW) {
-      downKeyPressed = true;
+        downKeyPressed = true;
     }
-  }
-  
-  function keyReleased() {
+}
+
+function keyReleased() {
     if (keyCode === LEFT_ARROW) {
-      leftKeyPressed = false;
+        leftKeyPressed = false;
     } else if (keyCode === RIGHT_ARROW) {
-      rightKeyPressed = false;
+        rightKeyPressed = false;
     } else if (keyCode === UP_ARROW) {
-      upKeyPressed = false;
+        upKeyPressed = false;
     } else if (keyCode === DOWN_ARROW) {
-      downKeyPressed = false;
+        downKeyPressed = false;
     }
-  }
-  
-  class Character {
+}
+
+class Character {
     constructor(x, y, size) {
-      this.x = x;
-      this.y = y;
-      this.size = size;
+        this.x = x;
+        this.y = y;
+        this.size = size;
     }
-  
+
     display() {
-      image(characterImg, this.x, this.y, this.size, this.size);
+        image(characterImg, this.x, this.y, this.size, this.size);
     }
-  }
+}
+
+class Tile {
+    constructor(texture, x, y, tileSize, tileID) {
+        this.texture = texture;
+        this.x = x;
+        this.y = y;
+        this.tileSize = tileSize;
+        this.tileID = tileID;
+    }
+
+    display() {
+        noStroke();
+        image(this.texture, this.x, this.y, this.tileSize);
+    }
+
+    debug() {
+        // TILE
+        stroke(245);
+        noFill();
+        rect(this.x, this.y, this.tileSize, this.tileSize);
+
+        // LABEL
+        noStroke();
+        fill(255);
+        textAlign(LEFT, TOP);
+        text(this.tileID, this.x, this.y);
+    }
+}
