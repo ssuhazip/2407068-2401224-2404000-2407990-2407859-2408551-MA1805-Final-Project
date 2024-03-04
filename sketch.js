e736cc8eb37162e14fd51dcb20170e578a040c0b
hanlacson <leelacs24@gmail.com>
2024-03-04 17:33:01 +0000

    Added map details

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//INITIALISE TILEMAP VARIABLES
 let tilemap = [];
 let numDown = 10;
 let numAcross = 10;
@@ -5,60 +6,64 @@ let tileSize = 50;
 
 let textures = [];
 let graphicsMap = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, ],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, ],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+    
+]
 
 let tileRules = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-];
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, ],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, ],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, ],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, ]
+]
 
-let characterImg;
-let character;
-let playerSpeed = 8;
-let leftKeyPressed = false;
-let rightKeyPressed = false;
-let upKeyPressed = false;
-let downKeyPressed = false;
+//INITIALISE PLAYER VARIABLES
+let player;
+let playerSprite;
+let playerSpeed = 5;
+let playerSize = tileSize;
+
 
 function preload() {
     textures[0] = loadImage("path.png");
     textures[1] = loadImage("wall.png");
-    characterImg = loadImage('player.png');
+
+    //player sprite
+    playerSprite = loadImage("player.png");
 }
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(800, 800);
 
     let tileID = 0;
     for (let across = 0; across < numAcross; across++) {
         tilemap[across] = [];
         for (let down = 0; down < numDown; down++) {
-            let x = across * tileSize;
-            let y = down * tileSize;
-            let textureNum = graphicsMap[down][across];
-            tilemap[across][down] = new Tile(textures[textureNum], x, y, tileSize, tileID);
+           
+            let textureNum;
+           textureNum = graphicsMap[down][across];
+           tilemap[across][down] = new Tile(textures[textureNum], across, down, tileSize, tileID);
+
             tileID++;
         }
+        console.log(graphicsMap[1][8])
     }
-    character = new Character(width / 5, height / 5, 60); // Character's initial position and size
+player = new Player(playerSprite, 3, 4, tileSize, playerSpeed, tileSize, tileRules);
 }
 
 function draw() {
@@ -70,80 +75,133 @@ function draw() {
             tilemap[across][down].debug();
         }
     }
-
-    // Move character
-    if (leftKeyPressed && character.x > 0) {
-        character.x -= speed;
-    }
-    if (rightKeyPressed && character.x < width - character.size) {
-        character.x += speed;
-    }
-    if (upKeyPressed && character.y > 0) {
-        character.y -= speed;
-    }
-    if (downKeyPressed && character.y < height - character.size) {
-        character.y += speed;
-    }
-    character.display();
+    player.display();
+    player.move();
 }
 
 function keyPressed() {
-    if (keyCode === LEFT_ARROW) {
-        leftKeyPressed = true;
-    } else if (keyCode === RIGHT_ARROW) {
-        rightKeyPressed = true;
-    } else if (keyCode === UP_ARROW) {
-        upKeyPressed = true;
-    } else if (keyCode === DOWN_ARROW) {
-        downKeyPressed = true;
-    }
-}
-
-function keyReleased() {
-    if (keyCode === LEFT_ARROW) {
-        leftKeyPressed = false;
-    } else if (keyCode === RIGHT_ARROW) {
-        rightKeyPressed = false;
-    } else if (keyCode === UP_ARROW) {
-        upKeyPressed = false;
-    } else if (keyCode === DOWN_ARROW) {
-        downKeyPressed = false;
-    }
+    player.setDirection();
 }
 
 class Tile {
-    constructor(texture, x, y, tileSize, tileID) {
+    constructor(texture, across, down, tileSize, tileID) {
         this.texture = texture;
-        this.x = x;
-        this.y = y;
+        this.across = across;
+        this.down = down;
+        this.xPos = across * tileSize;
+        this.yPos = down * tileSize;
         this.tileSize = tileSize;
         this.tileID = tileID;
     }
 
     display() {
         noStroke();
-        image(this.texture, this.x, this.y, this.tileSize);
+        image(this.texture, this.xPos, this.yPos, this.tileSize);
     }
 
     debug() {
+        //TILE
         stroke(245);
         noFill();
-        rect(this.x, this.y, this.tileSize, this.tileSize);
+        rect(this.xPos, this.yPos, this.tileSize, this.tileSize);
+
+        //LABEL
         noStroke();
         fill(255);
         textAlign(LEFT, TOP);
-        text(this.tileID, this.x, this.y);
+        
+        text(this.tileID, this.xPos, this.yPos);
     }
+ 
 }
 
-class Character {
-    constructor(x, y, size) {
-        this.x = x;
-        this.y = y;
+class Player {
+    constructor(sprite, startAcross, startDown, size, speed, tileSize, tileRules) {
+        this.sprite = sprite;
+        this.across = startAcross;
+        this.down = startDown;
         this.size = size;
+        this.speed = speed;
+        this.tileSize = tileSize;
+        this.tileRules = tileRules;
+        this.xPos = this.across * tileSize;
+        this.yPos = this.down * tileSize;
+
+        this.dirX = 0;
+        this.dirY = 0;
+
+        this.isMoving = false;
+        
+        this.tx = this.xPos;
+        this.ty = this.yPos;
+
+    }
+    setDirection() {
+        if (!this.isMoving) {
+            //UP
+            if(key === "w") {
+                this.dirX = 0;
+                this.dirY = -1;
+            }
+            //DOWN
+            if(key === "s") {
+                this.dirX = 0;
+                this.dirY = 1;
+            }
+            //LEFT
+            if(key === "a") {
+                this.dirX = -1;
+                this.dirY = 0;
+            }
+            //RIGHT
+            if(key === "d") {
+                this.dirX = 1;
+                this.dirY = 0;
+            }
+
+            this.checkTargetTile();    
+        }
+    } 
+
+    checkTargetTile() {
+        this.across = Math.floor(this.xPos / this.tileSize);
+        this.down = Math.floor(this.yPos / this.tileSize);
+
+        let nextTileHorizontal = this.across + this.dirX;
+        let nextTileVertical = this.down +this.dirY;
+
+        if (
+            nextTileHorizontal >= 0 && //top of map
+            nextTileHorizontal < numAcross &&
+            nextTileVertical >= 0 &&
+            nextTileVertical < numDown 
+           ) {
+
+            if(this.tileRules[nextTileVertical][nextTileHorizontal] !=1) {
+                this.tx = nextTileHorizontal * tileSize;
+                this.ty = nextTileVertical * tileSize;
+
+                this.isMoving = true;
+            }
+        }
     }
 
-    display() {
-        image(characterImg, this.x, this.y, this.size, this.size);
+    move() {
+        if(this.isMoving) {
+            this.xPos += this.speed * this.dirX;
+            this.yPos += this.speed * this.dirY;
+
+            if(this.xPos === this.tx && this.yPos === this.ty) {
+                this.isMoving = false;
+                this.dirX = 0;
+                this.dirY = 0;
+            }
+        }
+
+    }
+
+display() {
+    imageMode(CORNER);
+    image(this.sprite, this.xPos, this.yPos, this.size, this.size);
     }
 }
